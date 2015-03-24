c37b55d9c848663e6e39504f728dff85bdceba2c
Marc Laval <mlaval@amadeus.com>
2015-03-25 00:52:53 +0100

    Animations for carousel

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -22,21 +22,49 @@ import {EventEmitter} from 'angular2/src/core/annotations/di';
 export class Carousel {
   constructor(@EventEmitter('indexchange') emitter:Function) {
     this.emitter = emitter;
-    this.activeIndex = 0;
+    this.activeIndex = -1;
     this.slides = [];
     this.wrap = true;
     this._interval = 5000;
     this.pause = "hover",
     this.timerId = null;
+    this.transitionEnd = this.noTransition ? false : getTransitionEnd();
+    this._isToRight = true;
+    this._isChangingSlide = false;
     this._startCycling();
   }
   set index(newValue) {
-    if (newValue >= 0 && newValue <= this.slides.length - 1) {
-      this.activeIndex = newValue;
-      this.slides.forEach((slide) => {
-        slide.refresh(this);
-      });
+    if (!this._isChangingSlide && newValue != this.activeIndex && newValue >= 0 && newValue <= this.slides.length - 1) {
+      this._isChangingSlide = true;
+      var currentSlide = this.slides[this.activeIndex];
+      var nextSlide = this.slides[newValue];
+      if (this.activeIndex == -1) {
+        this._finalizeTransition(null, nextSlide, newValue);
+      } else if (this.transitionEnd) {
+        nextSlide.prepareAnimation(this._isToRight ? "next" : "prev");
+        setTimeout(() => {
+          currentSlide.animate(this._isToRight ? "left" : "right");
+          nextSlide.animate(this._isToRight ? "left" : "right");
+          var endAnimationCallback = (event) => {
+            currentSlide.el.domElement.removeEventListener(this.transitionEnd, endAnimationCallback, false);
+            this._finalizeTransition(currentSlide, nextSlide, newValue);
+          };
+          currentSlide.el.domElement.addEventListener(this.transitionEnd, endAnimationCallback, false);
+        }, 30);
+      } else {
+        this._finalizeTransition(currentSlide, nextSlide, newValue);
+      }
     }
+  }
+  _finalizeTransition(currentSlide, nextSlide, newValue) {
+    if (currentSlide) {
+      currentSlide.deactivate();
+      currentSlide.cleanAfterAnimation();
+    }
+    nextSlide.activate();
+    nextSlide.cleanAfterAnimation();
+    this.activeIndex = newValue;
+    this._isChangingSlide = false;
     this.emitter(this.activeIndex);
   }
   set interval(newValue) {
@@ -48,17 +76,20 @@ export class Carousel {
     this.slides.push(slide);
   }
   navigateTo(newIndex) {
+    this._isToRight = newIndex > this.activeIndex;
     this.index = newIndex;
   }
   prev() {
     if (this.hasPrev()) {
       var prevIndex = this.activeIndex - 1 < 0 ? this.slides.length - 1 : this.activeIndex - 1;
+      this._isToRight = false;
       this.index = prevIndex;
     }
   }
   next() {
     if (this.hasNext()) {
       var nextIndex = (this.activeIndex + 1) % this.slides.length;
+      this._isToRight = true;
       this.index = nextIndex;
     }
   }
@@ -82,7 +113,6 @@ export class Carousel {
     this.timerId = null;
   }
   toggleOnHover() {
-    console.log("Toggle")
     if (this.pause === "hover") {
       if (this.timerId) {
         this._stopCycling();
@@ -105,14 +135,24 @@ export class CarouselSlide {
     DOM.addClass(el.domElement, "item");
     this.index = carousel.slides.length;
     carousel.registerSlide(this);
-    this.refresh(carousel);
   }
-  refresh(carousel) {
-    if (this.index == carousel.activeIndex) {
-      DOM.addClass(this.el.domElement, "active");
-    } else {
-      DOM.removeClass(this.el.domElement, "active");
-    }
+  activate() {
+    DOM.addClass(this.el.domElement, "active");
+  }
+  deactivate(){
+    DOM.removeClass(this.el.domElement, "active");
+  }
+  prepareAnimation(className) {
+    DOM.addClass(this.el.domElement, className);
+  }
+  animate(className) {
+    DOM.addClass(this.el.domElement, className);
+  }
+  cleanAfterAnimation() {
+    DOM.removeClass(this.el.domElement, "left");
+    DOM.removeClass(this.el.domElement, "next");
+    DOM.removeClass(this.el.domElement, "right");
+    DOM.removeClass(this.el.domElement, "prev");
   }
 }
 
